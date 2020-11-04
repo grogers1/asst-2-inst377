58ee5cbf11742e90f13a89e954fadde902f504c3
choang07 <choang07@tepmail.umd.edu>
2020-11-03 23:22:58 -0500

    chi's html and js first commit

---
+++ public/script.js
@@ -0,0 +1,44 @@
+function findMatches(wordToMatch, restaurantList) {
+  // process your restaurants here!
+  const list = restaurantList.filter(restaurant => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return restaurant.category.match(regex) || restaurant.zip.match(regex);
+  });
+  return list;
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, restaurants);
+  const html = matchArray.map(restaurant => {
+    return `
+      <li>
+        <span class="name">${restaurant.name}</span><br/>
+        <span class="category">${restaurant.category}</span><br/>
+        <span class="address_line_1">${restaurant.address_line_1}</span><br/>
+        <span class="city">${restaurant.city}</span><br/>
+        <span class="zip">${restaurant.zip}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+const restaurants = [];
+fetch('/api', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: ''
+})
+  .then((fromServer) => fromServer.json())
+  .then((jsonFromServer) => restaurants.push(...jsonFromServer))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const searchInput = document.querySelector('.textentry');
+const suggestions = document.querySelector('#navContainer');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
